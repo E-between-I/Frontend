c3b794bb296e8b4a2045b64d97542175385152c1
inung1004 <dksdbswl10430@naver.com>
2024-01-28 02:07:56 +0900

    feat :: 다른 방법으로 로그인하기 페이지 퍼블리싱

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import LoginPage from "../pages/LoginPage";
+import AnotherLoginPage from "../pages/AnotherLoginPage";
 
 const MainRouter = () => {
   return (
     <Routes>
       <Route element={<MainPage />} path="/" />
       <Route element={<LoginPage />} path="/login" />
+      <Route element={<AnotherLoginPage />} path="/another_login" />
     </Routes>
   );
 };
