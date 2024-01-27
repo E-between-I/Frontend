fdfe470943516059d7ec5f805b4ced3f8a6607b7
inung1004 <dksdbswl10430@naver.com>
2024-01-27 20:07:43 +0900

    setting :: theme, global style 세팅

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -2,6 +2,10 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   * {
+    font-family: "Pretendard", sans-serif !important;
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+    -webkit-touch-callout: none;
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
   }
