4858185610fbc65662156f530858aeca922d407c
inung1004 <dksdbswl10430@naver.com>
2024-01-28 05:27:33 +0900

    add :: 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,5 @@ export { default as Logo } from "./Logo.png";
 export { default as MainTitle } from "./main.svg";
 export { default as BackgroundImg } from "./BackgroundImg.png";
 export { default as GoogleLogo } from "./GoogleLogo.svg";
+export { default as ProfileImg } from "./ProfileImg.png";
+export { default as Check } from "./Check.svg";
