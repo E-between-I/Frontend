f17ae7f0e86ebc4dc4b9fa45821d2c05bacefb56
inung1004 <dksdbswl10430@naver.com>
2024-01-27 23:05:25 +0900

    fix :: 컴포넌트 분리

--- src/components/Main/Header/index.tsx
+++ src/components/Main/Header/index.tsx
@@ -1,48 +1,12 @@
 import styled from "styled-components";
 import { Logo } from "../../../assets";
-import { useLocation, useNavigate } from "react-router-dom";
-import { removeToken } from "../../../utils/functions/TokenManager";
-import { HEADER_LIST } from "../../../constants/main";
+import Texts from "./Text";
 
 const Header = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const onTextClicckHandle = (route: string) => {
-    navigate(route);
-  };
-
-  const onLogoutClickHandle = () => {
-    removeToken();
-  };
-
-  const onLoginClickHandle = () => {
-    navigate("/login");
-  };
-
   return (
     <Container>
       <img src={Logo} alt="로고입니다." />
-      <div>
-        {HEADER_LIST.map((value) => (
-          <Text
-            onClick={() => onTextClicckHandle(value.route)}
-            isSelected={value.route === location.pathname}
-          >
-            {value.title}
-          </Text>
-        ))}
-        {localStorage.getItem("accessToken") ? (
-          <Text onClick={onLogoutClickHandle}>로그아웃</Text>
-        ) : (
-          <Text
-            onClick={onLoginClickHandle}
-            isSelected={"/login" === location.pathname}
-          >
-            로그인
-          </Text>
-        )}
-      </div>
+      <Texts />
     </Container>
   );
 };
@@ -62,16 +26,6 @@ const Container = styled.div`
     display: flex;
     gap: 30px;
   }
-`;
-
-const Text = styled.p<{ isSelected?: boolean }>`
-  cursor: pointer;
-  font-size: 18px;
-  font-weight: 400;
-  line-height: 24px;
-  color: ${(props) =>
-    props.isSelected ? props.theme.colors.main : props.theme.colors.gray600};
-  text-decoration-line: ${(props) => props.isSelected && "underline"};
 `;
 
 export default Header;
