1cdddce78eb64e9bf3b2dd597bd3f576f7b152dd
Tech5818 <pqow5818@gmail.com>
2024-01-28 06:07:47 +0900

    :sparkles: 강의 페이지 구현

---
+++ src/style/Lecture/Lecture.style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledLecture = styled.div`
+  width: 300px;
+`;
+
+export const StyledLectureImg = styled.img`
+  width: 300px;
+`;
+
+export const StyledLectureTitle = styled.p`
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+export const StyledLectureWriter = styled.p`
+  font-size: 18px;
+  color: #0066ff;
+  font-weight: 700;
+  margin-top: 8px;
+`;
--- src/style/Lecture/LecturePage.style.ts
+++ src/style/Lecture/LecturePage.style.ts
@@ -3,13 +3,45 @@ import styled from "styled-components";
 export const StyledLecturePage = styled.div`
   height: calc(100vh - 57px);
   display: flex;
-  justify-content: center;
+  flex-flow: column;
   align-items: center;
+  gap: 20px;
 `;
 
 export const StyledLecturePageContainer = styled.div`
   display: flex;
   flex-flow: column;
+  padding-top: 60px;
+  gap: 25px;
 `;
 
-export const StyledLectureTitle = styled.p``;
+export const StyledLectureTitle = styled.p`
+  font-size: 48px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+export const StyledLectureTabMenu = styled.div`
+  display: flex;
+  gap: 100px;
+`;
+
+export const StyledLectureTab = styled.p<{ $isSelected: boolean }>`
+  font-size: 28px;
+  padding: 5px 15px;
+  color: ${({ $isSelected }) => ($isSelected ? "#0066ff" : "")};
+  font-weight: ${({ $isSelected }) => ($isSelected ? "600" : "")};
+  cursor: pointer;
+  &:hover {
+    color: #0066ff;
+    font-weight: 600;
+  }
+`;
+
+export const StyledLecturePageContent = styled.div<{ $isSelected: boolean }>`
+  display: ${({ $isSelected }) => ($isSelected ? "flex" : "none")};
+  flex-wrap: wrap;
+  max-width: 1400px;
+  justify-content: center;
+  gap: 10px;
+`;
