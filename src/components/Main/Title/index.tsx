73a6cc5136ab2435b1516ceceeacb97a8874dd4b
유지민 <a01040987587@gmail.com>
2024-01-27 21:10:34 +0900

    feat: title 퍼블리싱

---
+++ src/components/Main/Title/index.tsx
@@ -0,0 +1,11 @@
+import MainImage from "../../../assets/main.svg";
+
+const Title = () => {
+  return (
+    <div>
+      <img src={MainImage} alt="error" />
+    </div>
+  );
+};
+
+export default Title;
