7d415f608762fb4f958ce6f8744fc459dfa72cad
inung1004 <dksdbswl10430@naver.com>
2024-01-28 13:41:31 +0900

    ㅇㄹㄴ

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,4 +1,3 @@
-import { useParams } from "react-router-dom";
 import {
   StyledCommentList,
   StyledCommnetSection,
@@ -22,54 +21,33 @@ import {
   StyledCommentInput,
   StyledCommentInputSection,
 } from "../style/Comment/Comment.style";
-import { instance } from "../constraints/axiosIntersepter/userIntersepter";
-import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const DetailPage = () => {
-  const [data, setData] = useState();
-  const { type, id } = useParams<{ type: string; id: string }>();
-
-  const getDetailQuestion = async () => {
-    await instance.get(`/post/${type}/${id}`).catch((res) => {
-      setData(res);
-      console.log("성공이다이쉐꺄");
-    });
-  };
-
-  // author: "바보";
-  // content: "제발탈출시켜줘";
-  // date: {
-  //   $date: "2024-01-28T04:39:03.961Z";
-  // }
-  // like: 100;
-  // title: "삼성전자 9만원에 몰려서 아직 못나온 썰 푼다";
-  // _id: {
-  //   $oid: "65b55bd75d64ca547ebdd54d";
-  // }
-
-  console.log(data);
-
-  useEffect(() => {
-    getDetailQuestion();
-    console.log(data);
-  }, []);
-
   return (
     <>
       <StyledDetailPage>
         <StyledDetailPageContainer>
-          {/* <StyledProfile>
+          <StyledProfile>
             <StyledProfileImg src="https://freesvg.org/img/abstract-user-flat-4.png" />
-            <StyledWriter>{data?.author}</StyledWriter>
+            <StyledWriter>바보</StyledWriter>
           </StyledProfile>
           <StyledContent>
-            <StyledTitle>{data?.title}</StyledTitle>
-            <StyledText>{data?.content}</StyledText>
-            <StyledLikeSection>
+            <StyledTitle>
+              삼성전자 9만원에 몰려서 아직 못나온 썰 푼다
+            </StyledTitle>
+            <StyledText>
+              이거 보여주려고 어그로 끌었다. 사실 삼성전자 레전드다.
+            </StyledText>
+            <StyledLikeSection
+              onClick={() =>
+                toast.error("로그인을 해주세요.", { duration: 1000 })
+              }
+            >
               <StyledLikeContainer>
                 <StyledLikeImg src="https://icones.pro/wp-content/uploads/2021/04/icone-noire-noir.png" />
               </StyledLikeContainer>
-              <StyledLikeCount>{data?.like}</StyledLikeCount>
+              <StyledLikeCount>3</StyledLikeCount>
             </StyledLikeSection>
             <StyledCommnetSection>
               <StyledCommnetTitle>댓글</StyledCommnetTitle>
@@ -78,11 +56,16 @@ export const DetailPage = () => {
                 <Comment />
               </StyledCommentList>
               <StyledCommentInputSection>
-                <StyledCommentInput placeholder="댓글 내용을 입력해 주세요!" />
+                <StyledCommentInput
+                  placeholder="댓글 내용을 입력해 주세요!"
+                  onClick={() =>
+                    toast.error("로그인을 해주세요.", { duration: 1000 })
+                  }
+                />
                 <StyledCommentButton>올리기</StyledCommentButton>
               </StyledCommentInputSection>
             </StyledCommnetSection>
-          </StyledContent> */}
+          </StyledContent>
         </StyledDetailPageContainer>
       </StyledDetailPage>
     </>
